4945a0cf124a11225fea5bc07b30a35d83d23f5d
sooj36 <sooj2023@gmail.com>
2023-09-19 15:48:30 +0900

    MainActivity.kt, DetailActivity.kt LOG.d

--- app/src/main/java/com/example/lifecycle/DetailActivity.kt
+++ app/src/main/java/com/example/lifecycle/DetailActivity.kt
@@ -1,4 +1,53 @@
 package com.example.lifecycle
 
-class DetailActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import android.util.Log
+import android.view.MenuItem
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+        val toMain = findViewById<Button>(R.id.to_main)
+        toMain.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("sooj", "onStart()_In_detail")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("sooj","onResume()_In_detail")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("sooj","onPause()_In_detail")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("sooj","onStop()_In_detail")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("sooj","onDestroy()_In_detail")
+    }
+
 }
+
+
+
--- app/src/main/java/com/example/lifecycle/MainActivity.kt
+++ app/src/main/java/com/example/lifecycle/MainActivity.kt
@@ -1,16 +1,26 @@
 package com.example.lifecycle
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toDetail = findViewById<Button>(R.id.to_detail)
+        toDetail.setOnClickListener {
+            val intent = Intent(this, DetailActivity::class.java)
+            startActivity(intent)
+            finish()
+            Log.d("sooj", "to_detail_btn")
+        }
     }
 
-    //////
+
     override fun onStart() {
         Log.d("sooj", "onStart()")
         super.onStart()
